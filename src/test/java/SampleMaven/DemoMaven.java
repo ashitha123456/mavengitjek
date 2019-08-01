70e9efbad7f021d413b23d95e31aa8acad13bf76
ashitha123456 <ashithashetty064@gmail.com>
2019-08-01 12:35:10 +0530

    first commit

---
+++ src/test/java/SampleMaven/DemoMaven.java
@@ -0,0 +1,13 @@
+package SampleMaven;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class DemoMaven {
+@Test
+	public static void run() {
+		Reporter.log("hello",true);
+
+	}
+
+}
---
+++ src/test/java/SampleMaven/Sample.java
@@ -0,0 +1,13 @@
+package SampleMaven;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Sample {
+	@Test
+	public void demo()
+	{
+		Reporter.log("hello",true);
+	}
+
+}
